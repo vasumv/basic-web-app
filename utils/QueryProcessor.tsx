a62ee1735843ced6af2ac34a02e5831a77a45cfc
Vasu Vikram <vvikram@andrew.cmu.edu>
2023-09-15 17:28:34 -0400

    Linter

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -23,24 +23,27 @@ export default function QueryProcessor(query: string): string {
 
     // match[1] will contain num1
     // match[2] will contain num2
-    const num1 = Number(match[1]); 
-    const num2 = Number(match[2]);
+    if (match != null) {
+      const num1 = Number(match[1]); 
+      const num2 = Number(match[2]);
 
-    return (
-      String(num1 + num2)
-    );
+      return (
+        String(num1 + num2)
+      );
+    }
   }
   if (query.toLowerCase().includes("is the largest")) {
     const numbers = query.match(/\d+/g); 
 
-
     // Convert to numbers
-    const num1 = parseInt(numbers[0]); 
-    const num2 = parseInt(numbers[1]);
-    const num3 = parseInt(numbers[2]);
-    return (
-      Math.max(num1, num2, num3)
-    );
+    if (numbers != null) {
+      const num1 = parseInt(numbers[0]); 
+      const num2 = parseInt(numbers[1]);
+      const num3 = parseInt(numbers[2]);
+      return (
+        String(Math.max(num1, num2, num3)
+      ));
+    }
   }
 
   return "";
